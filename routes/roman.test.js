89813f667fd97f3ca8583a64bcd0b77dbd739bd0
kennnaaay <kenny.luo@ibm.com>
2023-07-03 17:16:07 -0400

    add more unit tests

--- routes/roman.test.js
+++ routes/roman.test.js
@@ -23,8 +23,40 @@ describe('Roman Numerals Route', function() {
       });
   });  
 
+  it('should return the correct Roman numeral for a valid input number', function(done) {
+    var inputNumber = '3999'; // Assuming the input is provided as a string
+    var expectedOutput = 'MMMCMXCIX';
+  
+    request(app)
+      .get('/romannumeral')
+      .query({ number: inputNumber })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+  
+        var result = res.body;
+        result.input = parseInt(result.input); // Parse the input as a number
+  
+        expect(result).to.deep.equal({ input: 3999, output: expectedOutput });
+        done();
+      });
+  });  
+
   it('should return an error for an invalid input number', function(done) {
-    var inputNumber = 'abc';
+    var inputNumber = '';
+
+    request(app)
+      .get('/romannumeral')
+      .query({ number: inputNumber })
+      .end(function(err, res) {
+        expect(res.status).to.equal(400);
+        expect(res.body).to.have.property('error');
+        done();
+      });
+  });
+
+  it('should return an error for an invalid input number', function(done) {
+    var inputNumber = 'qweqwe';
 
     request(app)
       .get('/romannumeral')
